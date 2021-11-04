fb260372a78b7a459e55a623a5a3bc1c32e2b302
wooseok0727 <crash2@naver.com>
2021-11-04 12:08:19 +0900

    java spring

--- Spring_01_Student/src/main/java/com/wooseok/student/controller/StudentController.java
+++ Spring_01_Student/src/main/java/com/wooseok/student/controller/StudentController.java
@@ -3,25 +3,27 @@ package com.wooseok.student.controller;
 import com.wooseok.student.models.StudentVO;
 import com.wooseok.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import java.util.List;
 
 @Controller
 public class StudentController {
 
-    /**
+    /*
      * @Autowired
      * Field Injection 방식은
      * 메모리 누수가 발생한다고 보고 되었고
      * 생성자 방식에 비해서 Null Point Exception 이 발생할 확률도 있다
+     *
+     * @Autowired
+     * private StudentService stService;
      */
-//    @Autowired
-//    private StudentService stService;
 
     /**
      * 생성자 주입방식
@@ -32,7 +34,7 @@ public class StudentController {
     private final StudentService stService;
 
     @Autowired
-    public StudentController(StudentService stService) {
+    public StudentController(@Qualifier("stServiceV1") StudentService stService) {
         this.stService = stService;
     }
 
