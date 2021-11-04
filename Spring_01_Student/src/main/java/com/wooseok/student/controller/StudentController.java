b9fa00be9109d9d12b1623da4bcc69429e0949f9
wooseok0727 <crash2@naver.com>
2021-11-04 12:15:29 +0900

    java spring

--- Spring_01_Student/src/main/java/com/wooseok/student/controller/StudentController.java
+++ Spring_01_Student/src/main/java/com/wooseok/student/controller/StudentController.java
@@ -33,6 +33,28 @@ public class StudentController {
      */
     private final StudentService stService;
 
+    /*
+        @Autowired 를 통한 자동 의존관계 주입시 여러개의 빈을 찾는다면?
+        오류가 발생!
+        스프링은 찾은 여러개의 빈(Bean)들 중 어떤 것을 할 기준이 없기 때문
+        여러개의 빈을 찾을 때 해결할 수 있는 3가지 방법
+        1) @Autowired 필드 명 매칭
+        2) @Qualifier
+        3) @Primary
+
+        @Primary 와 @Qualifier 를 적절히 사용하는 것이 가장 이상적
+        @Primary 는 하나의 구현체만을 사용하게 하는 것
+        @Qualifier 는 @Primary 보다 귀찮지만, 디테일하게 접근하고 사용할 수 있음
+        --> @Primary 보다 우선순위가 높다!
+        
+        예시: 코드에서 자주 사용하는 메인 데이터베이스 커넥션을 사용하는 빈이 있고,
+              코드에서 특별한 기능으로 가끔 사용하는 서브 데이터에비스의 커넥션이 있을 때
+              메인 데이터베이스 커넥션을 사용하는 빈
+               --> @Primary 로 기본설정
+
+             서브 데이터에비스의 커넥션을 사용하는 빈
+             -> @Qualifier 지정해서 명시적으로 획득!
+     */
     @Autowired
     public StudentController(@Qualifier("stServiceV1") StudentService stService) {
         this.stService = stService;
